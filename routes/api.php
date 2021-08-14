5b12f8cae32be4bce6288265779ddc658298d29b
Erfan Ahmed Siam <erfan.siam98@gmail.com>
2021-08-14 23:51:49 +0600

    Chuckles Commit

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+//Welcome TO this new branch!
+//I will be creating multi Auth For this project and then will merge it to master
