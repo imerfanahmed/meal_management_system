2fd206cd7b72771d74afdc5c0104215ee3a11e20
Erfan Ahmed Siam <erfan.siam98@gmail.com>
2022-04-16 11:34:22 +0600

    Upgraded to laravel 9

--- resources/views/dailyMeal.blade.php
+++ resources/views/dailyMeal.blade.php
@@ -19,32 +19,32 @@
     <div class="content-wrapper">
         <div class="row">
             <div class="col-md-12">
-                <h1>Current Month : {{Carbon::now()->format('F')}}</h1>
+                <h1>Current Month : {{Carbon::now()->toFormattedDateString()}}</h1>
                 {{-- <h1>Number of Meal this Month : <span id="meal Number"></span> </h1> --}}
-                <label for="fp-default">Default</label>
-                <input type="text" id="fp-default" class="form-control flatpickr-basic" placeholder="YYYY-MM-DD" />
+                {{-- <label for="fp-default">Default</label>
+                <input type="text" id="fp-default" class="form-control flatpickr-basic" placeholder="YYYY-MM-DD" /> --}}
                 <div class="card text-left">
                   <div class="card-body">
                     <table class="table">
                         <thead>
                             <tr>
                                 <th>Date</th>
-                                <th>Day</th>
+                                {{-- <th>Day</th> --}}
                                 <th>Meal Status</th>
                             </tr>
                         </thead>
-                        <tbody>
+                        <tbody> 
                             @foreach ($members as $member)
                                 <tr>
                                     {{-- @dump($member->meals[0]) --}}
                                     <td>{{$member->name}}</td>
 
 
-                                    <td>{{$member->meals->first()->meal_status ?? "Nothing"}}</td>
+                                    {{-- <td>{{$member->meals->first()->meal_status ?? "Nothing"}}</td> --}}
                                     <td>
                                         <div class="custom-control custom-switch custom-switch-success">
-                                            <input type="checkbox" class="custom-control-input"  checked onclick="numberOfChecked()"/>
-                                            <label class="custom-control-label" >
+                                            <input type="checkbox" checked class="custom-control-input" id="{{$loop->index}}" />
+                                            <label class="custom-control-label"for="{{$loop->index}}" >
                                                 <span class="switch-icon-left"><i data-feather="check"></i></span>
                                                 <span class="switch-icon-right"><i data-feather="x"></i></span>
                                             </label>
